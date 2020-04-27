6c7742909c388f632a35375724295cc4b3d634b3
volizik <kizilov.yura@gmail.com>
2020-04-28 00:15:18 +0300

    init

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
-const express = require('express')
-const app = express()
-const path = require('path')
-const port = process.env.PORT || 3001
+const express = require('express');
+const favicon = require('express-favicon');
+const path = require('path');
+const port = process.env.PORT || 8080;
 
-app.use('/', express.static(path.join(__dirname, 'public')))
+// здесь у нас происходит импорт пакетов и определяется порт нашего сервера
+const app = express();
+app.use(favicon(__dirname + '/build/favicon.png'));
 
-app.listen(port, () => console.log("Listening on Port", port))
+//здесь наше приложение отдаёт статику
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+
+//простой тест сервера
+app.get('/ping', function (req, res) {
+    return res.send('pong');
+});
+
+//обслуживание html
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+app.listen(port);
